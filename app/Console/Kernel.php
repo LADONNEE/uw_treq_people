01596a5d52497533c89711e0bece0a5cf7f22e8d
Nadhir <nadhir@ladonnee.com>
2022-12-09 15:30:30 +0100

    Kernel command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('update:persons')->dailyAt('15:40')->timezone('Europe/Berlin');;
     }
 
     /**
